c819342dc329306eece2fb8eeacec1a68ae4eafd
MohamedShedo <mohamedshedo@gmail.com>
2018-07-19 03:36:02 +0200

    New test cases

--- ttests/server.test.js
+++ ttests/server.test.js
@@ -3,7 +3,11 @@ const request = require('supertest');
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
-
+beforeEach((done)=>{
+    Todo.remove({}).then(()=>{
+        done();
+    });
+})
 describe('post /todo',()=>{
 
     it('should create new todo',(done)=>{
@@ -19,13 +23,27 @@ describe('post /todo',()=>{
         .end((err,res)=>{
             if(err){return done(err);}
 
-           Todo.findOne({text}).then((res)=>{
-               expect(res.text).toBe(text);
+           Todo.find().then((res)=>{
+            expect(res.length).toBe(1);   
+            expect(res[0].text).toBe(text);
                 done();
             })
             .catch((e)=>{
                 done(e);
              });
         });
+    });
+
+    it("should not create todo with invalid body",(done)=>{
+
+        request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err)=>{
+            if(err){return done(err);}
+
+            done();
+        })
     })
 });
